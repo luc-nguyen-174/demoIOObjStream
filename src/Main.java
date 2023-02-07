05c13f2cd3277e20a09854a7ed95cf572136040f
luc-nguyen'174 <louiskingvn@gmail.com>
2023-02-07 14:35:22 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.*;
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<User> users = new ArrayList<>();
+        users.add(new User(1,"abc",30));
+        users.add(new User(2,"def",31));
+        users.add(new User(3,"jadh",28));
+        writeFile(users,"demo.bin");
+        System.out.println(readFile("demo.bin"));
+    }
+
+    public static boolean writeFile(List<User> users , String filename) {
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename));
+            oos.writeObject(users);
+            return true;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static List<User> readFile(String filename){
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename));
+            return (List<User>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
